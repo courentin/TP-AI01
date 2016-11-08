1b99722e227fb5ab2fa836a415a4ab7921cae142
Corentin <ch2kn@free.fr>
2016-11-08 23:11:08 +0100

    Begin of remove

--- tp3.h
+++ tp3.h
@@ -20,6 +20,12 @@ void insert_begining_list(List *list, char *str);
 
 int insert_after_position(List *list, char *str, int p);
 
+int remove_el(List *list, int p);
+
 void display(List *list);
+
+int compare(char *str1, char *str2);
+
+Element *get_element_at_position(List *list, int p);
 
 #endif
