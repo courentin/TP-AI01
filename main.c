7efe0196794313f83e74cc8c706a7d082e15e7b5
Corentin <ch2kn@free.fr>
2016-11-08 16:57:43 +0100

    Clean main

--- main.c
+++ main.c
@@ -5,7 +5,10 @@ int main(int argc, char const *argv[]) {
   List *list = malloc(sizeof(List));
   initialize(list);
 
+  insert_begining_list(list, "123456789");
+  insert_begining_list(list, "12345678910");
 
+  display(list);
 
   return 0;
 }
